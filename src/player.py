4d471689fdf78ce95e32424adf8500985bbef2ea
cacheUp <bradb26@gmail.com>
2020-02-13 12:14:10 +0300

    fix bug

--- src/player.py
+++ src/player.py
@@ -1,8 +1,27 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-from adv import room
 from room import Room
+
+
+room = {
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
+
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east."""),
+
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+into the darkness. Ahead to the north, a light flickers in
+the distance, but there is no way across the chasm."""),
+
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+chamber! Sadly, it has already been completely emptied by
+earlier adventurers. The only exit is to the south."""),
+}
 
 
 class Player:
@@ -25,8 +44,6 @@ class Player:
         self.items = []
 
     def set_room(self, direction):
-        print("made it")
-        print(self.current_room)
         if self.current_room.name == "Outside Cave Entrance":
             print(direction)
             if direction == "n":
